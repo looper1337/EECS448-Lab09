7b7e05bb0dba54f2840bad0f32b57913047829b4
Trieuanh Nguyen <t969n947@ku.edu.com>
2020-11-15 23:55:00 -0600

    Final Final

--- Web_Store/myfirstprogram.php
+++ Web_Store/myfirstprogram.php
@@ -14,7 +14,6 @@
   echo "<tr><td>Chicken Wings</td><td>".$_POST["chicken"]."</td><td>$7.00</td><td>$".$_POST["chicken"]*7.00.".00</td>";
   echo "<tr><td>Chili Fries</td><td>".$_POST["fries"]."</td><td>$5.00</td><td>$".$_POST["fries"]*5.00.".00</td>";
   echo "<tr><td>Cheeseburger</td><td>".$_POST["cburger"]."</td><td>$6.00</td><td>$".$_POST["cburger"]*6.00.".00</td>";
-  //the $_POST for shipping is set very specifically to use it as an index into a type array and a price array
   echo "<tr><td>Shipping</td><td colspan='2'>".['7-Day', '3-Day', 'Overnight'][$_POST["shipping"]]."</td><td>$".[0 ,5, 50][$_POST['shipping']].".00</td>";
   echo "<tr><td colspan='3'>Total Cost</td><td>$".($_POST["chicken"]*7+($_POST["fries"]*5)+($_POST["cburger"]*6)+[0, 5, 50][$_POST["shipping"]]).".00</td>";
   echo "</tbody></table>"; 
